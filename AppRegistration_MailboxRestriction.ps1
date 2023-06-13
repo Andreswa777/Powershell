537c986797606a1664acd178c457e090153f2027
Andreswa777 <44929106+Andreswa777@users.noreply.github.com>
2023-06-13 20:29:27 +0200

    Add files via upload

---
+++ AppRegistration_MailboxRestriction.ps1
@@ -0,0 +1,14 @@
+﻿connect-ExchangeOnline
+
+Test-ApplicationAccessPolicy -Identity adm-andres@parkadvertising.co.za -AppId ac86f593-0a3d-4c09-b360-4ff9d1514a67
+Test-ApplicationAccessPolicy -Identity New#@mediashop.co.za -AppId ac86f593-0a3d-4c09-b360-4ff9d1514a67
+
+
+New-ApplicationAccessPolicy -AppId ac86f593-0a3d-4c09-b360-4ff9d1514a67 -PolicyScopeGroupId RoomResources@NahanaCG.onmicrosoft.com -AccessRight RestrictAccess -Description "Restrict this app to members of distribution group EvenUsers."
+
+Test-ApplicationAccessPolicy -Identity adm-andres@parkadvertising.co.za -AppId ac86f593-0a3d-4c09-b360-4ff9d1514a67
+Test-ApplicationAccessPolicy -Identity New#@mediashop.co.za -AppId ac86f593-0a3d-4c09-b360-4ff9d1514a67
+
+
+# Disconnect from Exchange
+Disconnect-ExchangeOnline
